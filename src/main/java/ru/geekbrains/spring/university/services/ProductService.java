6fc48aa107f74d4a363252188b53e7507abd2a54
Vladimir Neizhmakov <vovka_x@mail.ru>
2021-03-11 10:15:20 +0300

    HW8

--- src/main/java/ru/geekbrains/spring/university/services/ProductService.java
+++ src/main/java/ru/geekbrains/spring/university/services/ProductService.java
@@ -1,7 +1,10 @@
 package ru.geekbrains.spring.university.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.geekbrains.spring.university.model.Product;
 import ru.geekbrains.spring.university.repository.ProductRepository;
 
@@ -13,7 +16,25 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
-    public List<Product> getAll() {return productRepository.findAll();}
+    @Transactional
+    public List<Product> getAll(Sort.Direction sort, int page, int size) {
+        if (page < 0) {page = 0;}
+        int totalpage = productRepository.findAll(PageRequest.of(page, size)).getTotalPages();
+        if (page >= totalpage) {page = totalpage-1;}
+
+        try {
+            if (sort.isAscending()) {
+                return productRepository.findAll(PageRequest.of(page, size, Sort.by("cost").ascending())).toList();
+            }
+            if (sort.isDescending()) {
+                return productRepository.findAll(PageRequest.of(page, size, Sort.by("cost").descending())).toList();
+            }
+        } catch (NullPointerException e){
+            e.printStackTrace();
+        }
+        return productRepository.findAll(PageRequest.of(page, size)).toList();
+    }
+
     public List<Product> getAllByCostGreaterThan(double minCost){return productRepository.findAllByCostGreaterThan(minCost);}
     public List<Product> getAllByCostLessThan(double maxCost){return productRepository.findAllByCostLessThan(maxCost);}
     public List<Product> getAllByCostBetween(double minCost, double maxCost){return productRepository.findAllByCostBetween(minCost, maxCost);}
